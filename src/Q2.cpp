b0c7c449bc64104967c5b57338dc204f0f0e4b7b
glenndeocampo <46602079+glenndeocampo@users.noreply.github.com>
2019-03-01 21:54:44 +0800

    Add files via upload

---
+++ src/Q2.cpp
@@ -0,0 +1,54 @@
+/* ****************************************************************
+ * @author: <your name here>
+ * @app name: <your application name here>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    string product = "";
+    int amount=0;
+
+    cout << "Enter Product Name: " << endl;
+    cin >> product;
+
+    cout << "Enter Amount: " << endl;
+    cin >> amount;
+
+    cout << product << " Daily Budget: " << amount << endl;
+
+
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    
+    system("pause");
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+/* ****************************************************************
+ * @author: <your name here>
+ * @app name: <your application name here>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    string user ="";
+    string password ="";
+    string address ="";
+
+    cout << "User's name:" << endl;
+    cin >> user;
+
+    cout << "Password:" << endl;
+    cin >> password;
+
+    cout << "Address:" << endl;
+    cin >> address;
+
+    cout << "Hi, I am " << user << ". I live at " << address << "." << endl;
+
+
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    
+    system("pause");
+
+}
